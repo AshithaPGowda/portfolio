e99e8608a7820c45f41af94338da0728b6c4c52d
Ashitha Gowda <pvagowda@yahoo.in>
2024-12-22 16:44:49 +0530

    added menu colour schemes

--- src/app/colours.js
+++ src/app/colours.js
@@ -1,5 +1,16 @@
-const COLOURS = []
+const COLOURS = [];
 
-COLOURS.TEXT_COLOUR_LIGHT = '#1B1833'
+COLOURS.TEXT_COLOUR_LIGHT = "#1B1833";
+
+COLOURS.MENU_COLOURS_LIGHT = [
+    "#2E5077", // Education
+    "#4DA1A9", // Projects
+    "#FADA7A", // Work Experience
+];
+COLOURS.MENU_COLOURS_DARK = [
+    "#00A9E0",  // Electric Blue (for Education)
+    "#FFEA00",  // Bright Yellow (for Projects)
+    "#FF6B6B",  // Soft Coral (for Work Experience)
+];
 
 export default COLOURS;
