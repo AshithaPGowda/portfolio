0d1e413dbc97063d2e067aeac8a0c282b5bbc03b
Ashitha Gowda <pvagowda@yahoo.in>
2024-12-24 13:29:12 +0530

    changed the darkmode light mode icons

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useMediaQuery } from "@mui/material"; // Import for media query
-import { Menu, Close, Brightness7, Brightness2 } from "@mui/icons-material"; // Import icons
+import { useMediaQuery } from "@mui/material"; 
+import { Menu, Close } from "@mui/icons-material"; 
+import DarkModeTwoToneIcon from '@mui/icons-material/DarkModeTwoTone';
+import WbSunnyTwoToneIcon from '@mui/icons-material/WbSunnyTwoTone';
 import FrostedGlassSidebar from "./frostedSidebar";
 import MenuItem from "./menuIcons";
 import COLOURS from "../colours";
 
 export default function Header({ theme, toggleTheme }) {
-    const [isSidebarOpen, setSidebarOpen] = useState(false); // Sidebar starts closed
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
     const [activeSection, setActiveSection] = useState(null);
     const menuItems = ["Home", "Education", "My Work"];
     const [activeIndex, setActiveIndex] = useState(0);
@@ -17,35 +19,32 @@ export default function Header({ theme, toggleTheme }) {
     const isMobile = useMediaQuery("(max-width:600px)");
 
     const handleSidebarToggle = () => {
-        setSidebarOpen(!isSidebarOpen); // Toggle sidebar visibility
+        setSidebarOpen(!isSidebarOpen);
     };
 
     const handleSectionChange = (index) => {
-        setActiveSection(index); // Update the active section
+        setActiveSection(index);
     };
 
     const handleItemClick = (index) => {
-        console.log("Selection clicked, ", index); // Notify parent component of section change
+        console.log("Selection clicked, ", index);
         setActiveIndex(index);
     };
 
     const handleThemeToggle = () => {
-        setIsDarkMode(!isDarkMode); // Toggle dark mode
-        toggleTheme(); // Call the parent toggleTheme function
+        setIsDarkMode(!isDarkMode);
+        toggleTheme();
     };
 
     // Close sidebar when clicked outside
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
-                setSidebarOpen(false); // Close the sidebar if clicked outside
+                setSidebarOpen(false);
             }
         };
 
-        // Add event listener to detect outside clicks
         document.addEventListener("mousedown", handleClickOutside);
-
-        // Cleanup the event listener on unmount
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
         };
@@ -57,18 +56,17 @@ export default function Header({ theme, toggleTheme }) {
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "center",
-                padding: "20px",
-                backgroundColor: COLOURS[`BACKGROUND_${theme}`], // Dynamic background for the header
-                color: COLOURS[`TEXT_${theme}`], // Dynamic text color
-                position: "fixed", // Keep header fixed at the top
+                padding: "2vw",
+                backgroundColor: COLOURS[`BACKGROUND_${theme}`],
+                color: COLOURS[`TEXT_${theme}`],
+                position: "fixed",
                 top: 0,
                 left: 0,
                 right: 0,
                 zIndex: 10,
             }}
         >
-            {/* Placeholder for logo */}
-            <div style={{ width: "50px", height: "50px", borderRadius: "8px" }}>
+            <div style={{ width: "5vw", height: "8vh", borderRadius: "1vw" }}>
                 <img src="apg.png" alt="APG Logo" style={{ width: "100%", height: "100%" }} />
             </div>
 
@@ -83,30 +81,29 @@ export default function Header({ theme, toggleTheme }) {
                                 border: "none",
                                 cursor: "pointer",
                                 fontSize: "24px",
-                                color: COLOURS[`TEXT_COLOUR_${theme}`], // Icon color based on theme
+                                color: COLOURS[`TEXT_COLOUR_${theme}`],
                                 display: "flex",
                                 alignItems: "center",
                                 position: "relative",
                             }}
                         >
-                            {/* Add fade-in/out effect to the icons */}
                             <span
                                 style={{
                                     position: "absolute",
-                                    opacity: isDarkMode ? 1 : 0, // Show the correct icon based on theme
+                                    opacity: isDarkMode ? 1 : 0,
                                     transition: "opacity 0.3s ease-in-out",
                                 }}
                             >
-                                <Brightness2 fontSize="medium" />
+                                <WbSunnyTwoToneIcon fontSize="inherit" />
                             </span>
                             <span
                                 style={{
                                     position: "absolute",
-                                    opacity: isDarkMode ? 0 : 1, // Show the correct icon based on theme
+                                    opacity: isDarkMode ? 0 : 1,
                                     transition: "opacity 0.3s ease-in-out",
                                 }}
                             >
-                                <Brightness7 fontSize="medium" />
+                                <DarkModeTwoToneIcon fontSize="inherit" />
                             </span>
                         </button>
                     )}
@@ -118,35 +115,33 @@ export default function Header({ theme, toggleTheme }) {
                                 background: "transparent",
                                 border: "none",
                                 cursor: "pointer",
-                                fontSize: "30px", // Size of the icon
+                                fontSize: "30px",
                                 fontWeight: "bold",
-                                color: COLOURS[`TEXT_COLOUR_${theme}`], // Icon color
+                                color: COLOURS[`TEXT_COLOUR_${theme}`],
                                 display: "flex",
                                 alignItems: "center",
                             }}
                         >
-                            <Menu fontSize="large" />
+                            <Menu fontSize="inherit" />
                         </button>
                     )}
 
-                    {/* Sidebar component */}
                     <FrostedGlassSidebar
                         isOpen={isSidebarOpen}
                         activeSection={activeSection}
                         onClose={handleSectionChange}
-                        ref={sidebarRef} // Attach ref to the sidebar
+                        ref={sidebarRef}
                         theme={theme}
                     />
 
-                    {/* Close button positioned on top of sidebar (only visible when sidebar is open) */}
                     {isSidebarOpen && (
                         <button
                             onClick={handleSidebarToggle}
                             style={{
                                 position: "fixed",
-                                top: "20px", // Adjust based on where you want it
-                                right: "20px", // Place it on the right
-                                zIndex: 20, // Ensure it appears above the sidebar
+                                top: "20px",
+                                right: "20px",
+                                zIndex: 20,
                                 background: "transparent",
                                 border: "none",
                                 cursor: "pointer",
@@ -154,60 +149,59 @@ export default function Header({ theme, toggleTheme }) {
                                 color: COLOURS[`TEXT_COLOUR_${theme}`],
                             }}
                         >
-                            <Close fontSize="large" />
+                            <Close fontSize="inherit" />
                         </button>
                     )}
                 </div>
             ) : (
-                // Desktop View
                 <div style={{ display: "flex", gap: "1.5vw", alignItems: "center" }}>
-                    {isSidebarOpen ? null : ( // Only show the menu items when sidebar is closed
+                    {isSidebarOpen ? null : (
                         <>
-                                                    <button
-                                onClick={handleThemeToggle}
-                                style={{
-                                    background: "transparent",
-                                    border: "none",
-                                    cursor: "pointer",
-                                    marginLeft: "20px",
-                                    fontSize: "24px",
-                                    color: COLOURS[`TEXT_COLOUR_${theme}`], // Icon color based on theme
-                                    display: "flex",
-                                    alignItems: "center",
-                                    position: "relative",
-                                }}
-                            >
-                                {/* Add fade-in/out effect to the icons for desktop */}
-                                <span
-                                    style={{
-                                        position: "absolute",
-                                        opacity: isDarkMode ? 1 : 0, // Show the correct icon based on theme
-                                        transition: "opacity 0.3s ease-in-out",
-                                    }}
-                                >
-                                    <Brightness2 />
-                                </span>
-                                <span
-                                    style={{
-                                        position: "absolute",
-                                        opacity: isDarkMode ? 0 : 1, // Show the correct icon based on theme
-                                        transition: "opacity 0.3s ease-in-out",
-                                    }}
-                                >
-                                    <Brightness7 />
-                                </span>
-                            </button>
                             {menuItems.map((text, index) => (
                                 <MenuItem
                                     key={index}
                                     text={text}
                                     index={index}
-                                    isActive={activeIndex === index} // Check if the item is active
+                                    isActive={activeIndex === index}
                                     onClick={() => handleItemClick(index)}
                                     theme={theme}
                                     toggleTheme={toggleTheme}
                                 />
                             ))}
+
+<button
+                                onClick={handleThemeToggle}
+                                style={{
+                                    background: "transparent",
+                                    border: "none",
+                                    cursor: "pointer",
+                                    marginRight: "2vw",
+                                    fontSize: "2.5vw",
+                                    color: COLOURS[`ICON_COLOUR_${theme}`],
+                                    display: "flex",
+                                    alignItems: "center",
+                                    position: "relative",
+                                }}
+                            >
+                                <span
+                                    style={{
+                                        position: "absolute",
+                                        opacity: isDarkMode ? 1 : 0,
+                                        transition: "opacity 0.3s ease-in-out",
+                                    }}
+                                >
+                                    <WbSunnyTwoToneIcon fontSize="inherit" />
+                                </span>
+                                <span
+                                    style={{
+                                        position: "absolute",
+                                        opacity: isDarkMode ? 0 : 1,
+                                        transition: "opacity 0.3s ease-in-out",
+                                    }}
+                                >
+                                    <DarkModeTwoToneIcon fontSize="inherit" />
+                                </span>
+                            </button>
                         </>
                     )}
                 </div>
--- src/app/components/menuIcons.js
+++ src/app/components/menuIcons.js
@@ -1,63 +1,47 @@
 import React from "react";
-import { Box, ListItemText } from "@mui/material";
-import { Work, School, Business, ContactMail, Home } from "@mui/icons-material"; // Corrected icons
-import COLOURS from "../colours"; // Assuming this is where your colors are stored
+import { Box, Tooltip } from "@mui/material";
+import { Work, School, Business, ContactMail, Home } from "@mui/icons-material"; // Icons
+import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
+import SchoolTwoToneIcon from '@mui/icons-material/SchoolTwoTone';
+import TerminalTwoToneIcon from '@mui/icons-material/TerminalTwoTone';
+import COLOURS from "../colours";
 
 const menuIcons = {
-  Home: <Home />,
-  Education: <School />,
+  Home: <HomeTwoToneIcon />,
+  Education: <SchoolTwoToneIcon />,
   Projects: <Business />,
-  "My Work": <Work />,
+  "My Work": <TerminalTwoToneIcon />,
   "Contact Me": <ContactMail />,
 };
 
-const MenuItem = ({ text, index, isActive, onClick, theme, toggleTheme }) => {
-  console.log("In menuItems, theme = ")
-  const Icon = menuIcons[text] || null; // Get the icon based on text
-  const iconColor = theme === "LIGHT" ? COLOURS.MENU_COLOURS_LIGHT[index] || "#000" : COLOURS.MENU_COLOURS_DARK[index] || "#FFF"; // Icon color based on theme
+const MenuItem = ({ text, index, theme }) => {
+  const Icon = menuIcons[text] || null; // Get the icon based on the text
+  const iconColor =
+    theme === "LIGHT"
+      ? COLOURS.MENU_COLOURS_LIGHT[index] || "#000"
+      : COLOURS.MENU_COLOURS_DARK[index] || "#FFF"; // Icon color based on theme
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        padding: "10px 20px",
-        borderRadius: "8px",
-        cursor: "pointer",
-        backgroundColor: isActive ? "rgba(255, 255, 255, 0.4)" : "transparent", // Highlight active menu item
-        "&:hover": {
-          backgroundColor: theme === "LIGHT" ? "rgba(0, 0, 0, 0.1)" : "rgba(255, 255, 255, 0.2)", // Hover effect based on theme
-        },
-        boxShadow: isActive ? "0 4px 12px rgba(0, 0, 0, 0.1)" : "none", // Optional shadow for active state
-        transition: "background-color 0.3s ease, box-shadow 0.3s ease", // Smooth transitions
-      }}
-      onClick={() => onClick(index)} // Pass the index to the click handler
-    >
-      {/* Icon Section */}
-      {Icon && (
-        <Box
-          sx={{
-            marginRight: "10px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {React.cloneElement(Icon, { style: { color: iconColor } })} {/* Apply color to the icon */}
-        </Box>
-      )}
-
-      {/* Text Section */}
-      <ListItemText
-        primary={text}
-        primaryTypographyProps={{
-          color: theme === "LIGHT" ? COLOURS.TEXT_COLOUR_LIGHT : COLOURS.TEXT_COLOUR_DARK, // Dynamically set text color based on theme
-          fontWeight: "550", // Increased font weight for better visibility
-          fontSize: "16px",
+    <Tooltip title={text} placement="right"> {/* Tooltip displays the text */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "4vw",
+          height: "4vh",
+          borderRadius: "50%",
+          cursor: "pointer",
+          backgroundColor: "transparent",
+          "&:hover": {
+            backgroundColor: theme === "LIGHT" ? "rgba(0, 0, 0, 0.1)" : "rgba(255, 255, 255, 0.2)",
+          },
+          transition: "background-color 0.3s ease",
         }}
-      />
-      {console.log("theme",theme, COLOURS.TEXT_COLOUR_DARK)}
-    </Box>
+      >
+        {React.cloneElement(Icon, { style: { color: iconColor, fontSize: "2.5vw" } })} {/* Apply styles to the icon */}
+      </Box>
+    </Tooltip>
   );
 };
 
