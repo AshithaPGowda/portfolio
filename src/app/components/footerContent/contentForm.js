581d96f854d278d4155106359b6fbbb6f2eea25f
Ashitha Gowda <pvagowda@yahoo.in>
2025-01-23 13:31:18 -0500

    content form works for desktop

--- src/app/components/footerContent/contentForm.js
+++ src/app/components/footerContent/contentForm.js
@@ -11,18 +11,26 @@ const ContactForm = ({ open, onClose, theme, COLOURS }) => {
                     top: "50%",
                     left: "50%",
                     transform: "translate(-50%, -50%)",
-                    width: "80%",
-                    maxWidth: "500px",
+                    width: "90%",
+                    maxWidth: "600px",
                     bgcolor: COLOURS[`SECTION_COLOUR_${theme}`],
                     boxShadow: 24,
                     p: 4,
-                    borderRadius: "8px",
+                    borderRadius: "12px", // Soften edges for a more modern look
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
                 }}
             >
-                <Typography variant="h6" gutterBottom style={{ color: COLOURS[`TEXT_COLOUR_${theme}`] }} textAlign="center">
+                <Typography 
+                    variant="h6" 
+                    gutterBottom 
+                    style={{ color: COLOURS[`TEXT_COLOUR_${theme}`], textAlign: "center", marginBottom: "20px" }}>
                     Contact Me
                 </Typography>
-                <form>
+
+                <form style={{ width: "100%" }}>
                     <TextField
                         fullWidth
                         label="Name"
@@ -34,6 +42,7 @@ const ContactForm = ({ open, onClose, theme, COLOURS }) => {
                         }}
                         sx={{
                             "& .MuiOutlinedInput-root": {
+                                borderRadius: "8px", // Rounded input fields
                                 "& fieldset": {
                                     borderColor: COLOURS[`TEXT_COLOUR_${theme}`],
                                 },
@@ -44,6 +53,7 @@ const ContactForm = ({ open, onClose, theme, COLOURS }) => {
                                     borderColor: COLOURS[`TEXT_COLOUR_${theme}`],
                                 },
                             },
+                            marginBottom: "16px", // Space between fields
                         }}
                     />
                     <TextField
@@ -57,6 +67,7 @@ const ContactForm = ({ open, onClose, theme, COLOURS }) => {
                         }}
                         sx={{
                             "& .MuiOutlinedInput-root": {
+                                borderRadius: "8px",
                                 "& fieldset": {
                                     borderColor: COLOURS[`TEXT_COLOUR_${theme}`],
                                 },
@@ -67,6 +78,7 @@ const ContactForm = ({ open, onClose, theme, COLOURS }) => {
                                     borderColor: COLOURS[`TEXT_COLOUR_${theme}`],
                                 },
                             },
+                            marginBottom: "16px",
                         }}
                     />
                     <TextField
@@ -82,6 +94,7 @@ const ContactForm = ({ open, onClose, theme, COLOURS }) => {
                         }}
                         sx={{
                             "& .MuiOutlinedInput-root": {
+                                borderRadius: "8px",
                                 "& fieldset": {
                                     borderColor: COLOURS[`TEXT_COLOUR_${theme}`],
                                 },
@@ -92,17 +105,43 @@ const ContactForm = ({ open, onClose, theme, COLOURS }) => {
                                     borderColor: COLOURS[`TEXT_COLOUR_${theme}`],
                                 },
                             },
+                            marginBottom: "24px", // Extra space before buttons
                         }}
                     />
-                    <div style={{ marginTop: "20px", textAlign: "right" }}>
+                    <div style={{ width: "100%", display: "flex", justifyContent: "flex-end" }}>
                         <Button
-                            variant="contained"
+                            variant="outlined"
                             onClick={onClose}
-                            style={{ marginRight: "10px", color: COLOURS[`TEXT_COLOUR_${theme}`] }}
+                            style={{
+                                marginRight: "10px",
+                                color: COLOURS[`TEXT_COLOUR_${theme}`],
+                                borderColor: COLOURS[`TEXT_COLOUR_${theme}`],
+                            }}
+                            sx={{
+                                borderRadius: "8px",
+                                "&:hover": {
+                                    backgroundColor: COLOURS[`TEXT_COLOUR_${theme}`],
+                                    color: COLOURS[`SECTION_COLOUR_${theme}`],
+                                },
+                            }}
                         >
                             Cancel
                         </Button>
-                        <Button variant="contained" color="primary" style={{ color: COLOURS[`TEXT_COLOUR_${theme}`] }}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={onClose}
+                            style={{
+                                color: COLOURS[`TEXT_COLOUR_${theme}`],
+                                borderRadius: "8px",
+                            }}
+                            sx={{
+                                "&:hover": {
+                                    backgroundColor: COLOURS[`TEXT_COLOUR_${theme}`],
+                                    color: COLOURS[`SECTION_COLOUR_${theme}`],
+                                },
+                            }}
+                        >
                             Submit
                         </Button>
                     </div>
