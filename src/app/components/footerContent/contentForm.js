a95202600046ad18512e295aec63e50d62b6c031
Ashitha Gowda <pvagowda@yahoo.in>
2025-01-30 18:22:58 -0500

    validated email before sending

--- src/app/components/footerContent/contentForm.js
+++ src/app/components/footerContent/contentForm.js
@@ -1,168 +1,216 @@
 "use client";
 
-import { Modal, Box, Typography, Button, TextField } from "@mui/material";
+import { Modal, Box, Typography, Button, TextField, Snackbar, Alert } from "@mui/material";
 import { useState } from "react";
 
 const ContactForm = ({ open, onClose, theme, COLOURS }) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
+    const [success, setSuccess] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+
+    // Email validation function
+    const validateEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
 
     const handleSubmit = async (e) => {
-        e.preventDefault(); // Prevent default form submission behavior
+        e.preventDefault();
 
-        // Create the payload for backend
+        if (!validateEmail(email)) {
+            setEmailError(true);
+            return;
+        }
+
+        setEmailError(false);
+
         const formData = { name, email, message };
+
         try {
-            // Make an API call to your backend to send the email
             const response = await fetch('/api/sendEmail', {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
             });
-    
+
             const result = await response.json();
-    
+
             if (response.ok) {
                 console.log('Email sent successfully:', result);
-                // Optionally, you can show a success message or take any further action
+                setSuccess(true); // Show success snackbar
+
+                // Reset form fields
+                setName("");
+                setEmail("");
+                setMessage("");
+
+                setTimeout(() => {
+                    onClose(); // Close modal after showing success message
+                }, 2000);
             } else {
                 console.error('Error sending email:', result);
-                // Optionally, you can show an error message to the user
             }
         } catch (error) {
             console.error('Error sending email:', error);
-            // Handle errors like network issues
         }
-
-        onClose(); // Close the modal after submission
     };
 
     return (
-        <Modal open={open} onClose={onClose}>
-            <Box
-                sx={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    width: "90%",
-                    maxWidth: "600px",
-                    bgcolor: COLOURS[`SECTION_COLOUR_${theme}`],
-                    boxShadow: 24,
-                    p: 4,
-                    borderRadius: "12px",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <Typography
-                    variant="h6"
-                    gutterBottom
-                    sx={{ color: COLOURS[`TEXT_COLOUR_${theme}`], textAlign: "center", mb: 2 }}
+        <>
+            <Modal open={open} onClose={onClose}>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        width: "90%",
+                        maxWidth: "600px",
+                        bgcolor: COLOURS[`SECTION_COLOUR_${theme}`],
+                        boxShadow: 24,
+                        p: 4,
+                        borderRadius: "12px",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
                 >
-                    Contact Me
-                </Typography>
+                    <Typography
+                        variant="h6"
+                        gutterBottom
+                        sx={{ color: COLOURS[`TEXT_COLOUR_${theme}`], textAlign: "center", mb: 2 }}
+                    >
+                        Contact Me
+                    </Typography>
 
-                <form style={{ width: "100%" }} onSubmit={handleSubmit}>
-                    <TextField
-                        fullWidth
-                        label="Name"
-                        margin="normal"
-                        variant="outlined"
-                        value={name}  // Bound to the name state
-                        onChange={(e) => setName(e.target.value)}  // Updates the name state on change
-                        slotProps={{
-                            inputLabel: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
-                            input: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
-                        }}
-                        sx={{
-                            "& .MuiOutlinedInput-root": {
-                                borderRadius: "8px",
-                                "& fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
-                                "&:hover fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
-                                "&.Mui-focused fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
-                            },
-                            marginBottom: "16px",
-                        }}
-                    />
-                    <TextField
-                        fullWidth
-                        label="Email"
-                        margin="normal"
-                        variant="outlined"
-                        slotProps={{
-                            inputLabel: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
-                            input: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
-                        }}
-                        value={email}  // Bound to the name state
-                        onChange={(e) => setEmail(e.target.value)}  // Updates the name state on change
-                        sx={{
-                            "& .MuiOutlinedInput-root": {
-                                borderRadius: "8px",
-                                "& fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
-                                "&:hover fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
-                                "&.Mui-focused fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
-                            },
-                            marginBottom: "16px",
-                        }}
-                    />
-                    <TextField
-                        fullWidth
-                        label="Message"
-                        margin="normal"
-                        variant="outlined"
-                        multiline
-                        rows={4}
-                        slotProps={{
-                            inputLabel: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
-                            input: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
-                        }}
-                        value={message}  // Bound to the name state
-                        onChange={(e) => setMessage(e.target.value)}  // Updates the name state on change
-                        sx={{
-                            "& .MuiOutlinedInput-root": {
-                                borderRadius: "8px",
-                                "& fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
-                                "&:hover fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
-                                "&.Mui-focused fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
-                            },
-                            marginBottom: "24px",
-                        }}
-                    />
-                    <div style={{ width: "100%", display: "flex", justifyContent: "flex-end" }}>
-                        <Button
+                    <form style={{ width: "100%" }} onSubmit={handleSubmit}>
+                        <TextField
+                            fullWidth
+                            label="Name"
+                            margin="normal"
                             variant="outlined"
-                            onClick={onClose}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                             sx={{
-                                marginRight: "10px",
-                                color: COLOURS[`TEXT_COLOUR_${theme}`],
-                                borderColor: COLOURS[`TEXT_COLOUR_${theme}`],
-                                borderRadius: "8px",
-                                "&:hover": { backgroundColor: COLOURS[`TEXT_COLOUR_${theme}`], color: COLOURS[`SECTION_COLOUR_${theme}`] },
+                                "& .MuiOutlinedInput-root": {
+                                    borderRadius: "8px",
+                                    "& fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
+                                    "&:hover fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
+                                    "&.Mui-focused fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
+                                },
+                                marginBottom: "16px",
                             }}
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            variant="contained"
-                            onClick={handleSubmit}
+                            slotProps={{
+                                inputLabel: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
+                                input: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
+                            }}
+                        />
+                        <TextField
+                            fullWidth
+                            label="Email"
+                            margin="normal"
+                            variant="outlined"
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                                setEmailError(!validateEmail(e.target.value));
+                            }}
+                            error={emailError}
+                            helperText={emailError ? "Please enter a valid email address." : ""}
                             sx={{
-                                color: COLOURS[`TEXT_COLOUR_${theme}`],
-                                borderRadius: "8px",
-                                "&:hover": { backgroundColor: COLOURS[`TEXT_COLOUR_${theme}`], color: COLOURS[`SECTION_COLOUR_${theme}`] },
+                                "& .MuiOutlinedInput-root": {
+                                    borderRadius: "8px",
+                                    "& fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
+                                    "&:hover fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
+                                    "&.Mui-focused fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
+                                },
+                                marginBottom: "16px",
                             }}
-                        >
-                            Submit
-                        </Button>
-                    </div>
-                </form>
-            </Box>
-        </Modal>
+                            slotProps={{
+                                inputLabel: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
+                                input: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
+                            }}
+                        />
+                        <TextField
+                            fullWidth
+                            label="Message"
+                            margin="normal"
+                            variant="outlined"
+                            multiline
+                            rows={4}
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            sx={{
+                                "& .MuiOutlinedInput-root": {
+                                    borderRadius: "8px",
+                                    "& fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
+                                    "&:hover fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
+                                    "&.Mui-focused fieldset": { borderColor: COLOURS[`TEXT_COLOUR_${theme}`] },
+                                },
+                                marginBottom: "24px",
+                            }}
+                            slotProps={{
+                                inputLabel: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
+                                input: { style: { color: COLOURS[`TEXT_COLOUR_${theme}`] } },
+                            }}
+                        />
+                        <div style={{ width: "100%", display: "flex", justifyContent: "flex-end" }}>
+                            <Button
+                                variant="outlined"
+                                onClick={onClose}
+                                sx={{
+                                    marginRight: "10px",
+                                    color: COLOURS[`TEXT_COLOUR_${theme}`],
+                                    borderColor: COLOURS[`TEXT_COLOUR_${theme}`],
+                                    borderRadius: "8px",
+                                    "&:hover": {
+                                        backgroundColor: COLOURS[`TEXT_COLOUR_${theme}`],
+                                        color: COLOURS[`SECTION_COLOUR_${theme}`],
+                                    },
+                                }}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                variant="contained"
+                                type="submit"
+                                disabled={emailError || !email || !name || !message} // Disable button if invalid
+                                sx={{
+                                    color: COLOURS[`TEXT_COLOUR_${theme}`],
+                                    borderRadius: "8px",
+                                    "&:hover": {
+                                        backgroundColor: COLOURS[`TEXT_COLOUR_${theme}`],
+                                        color: COLOURS[`SECTION_COLOUR_${theme}`],
+                                    },
+                                }}
+                            >
+                                Submit
+                            </Button>
+                        </div>
+                    </form>
+                </Box>
+            </Modal>
+
+            {/* Snackbar for Success Message */}
+            <Snackbar
+                open={success}
+                autoHideDuration={3500}
+                onClose={() => setSuccess(false)}
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            >
+                <Alert
+                    onClose={() => setSuccess(false)}
+                    severity="success"
+                    sx={{ width: "100%" }}
+                >
+                    Message sent successfully! Acknowledgment sent to your email.
+                    If not received, please check your spam folder and mark it as a trusted sender.
+                </Alert>
+            </Snackbar>
+        </>
     );
 };
 
