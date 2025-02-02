4c76b2bc7ef25ed4fe52c83b9d90b7302ba0e1f8
Ashitha Gowda <pvagowda@yahoo.in>
2025-02-02 18:00:57 -0500

    now my information is a new section

---
+++ src/app/myInformation.js
@@ -0,0 +1,11 @@
+const CONSTANT = []
+
+CONSTANT.PARA1 = `I'm a full-stack developer who thrives on solving tough problems 🧩 and building high-performance systems 🚀. Backend engineering and APIs are my playground 🎯, and I love making things work seamlessly with Node.js, Next.js, React, and blockchain. At cnotes.in, I built a custom algorithm that predicts monthly billing for over 30,000 customers 💡— turning complexity into clarity. I'm always exploring how AI can push technology forward 🤖, and I thrive on collaboration, innovation, and challenges that make me think 🔥. When I'm not coding, I'm learning, experimenting, or finding new ways to level up 📚
+`
+CONSTANT.PARA2 = ` 💬 I'm a strong believer in collaboration and empathy, always striving to uplift those around me. If
+                    there's one thing I value deeply, it's meaningful connections and making a positive impact in every space
+                    I occupy. 🎨 Outside work, you'll find me immersed in art, experimenting in the kitchen, or on a quest to
+                    discover the best local coffee shops. Life, for me, is about crafting beautiful experiences—both in and
+                    outside the code.`
+CONSTANT.MY_NAME = "ASHITHA PARAMESHA GOWDA"
+export default CONSTANT;
