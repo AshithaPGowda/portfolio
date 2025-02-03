d7744fe9ca0633432b7cf30fb0760222b7e55583
Ashitha Gowda <pvagowda@yahoo.in>
2025-02-02 19:09:05 -0500

    added a loader to trigger loading when needed

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,13 +1,20 @@
 "use client";
 
-import {Suspense } from "react";
+import { Suspense, useState,useEffect } from "react";
 import ProjectSelection from "./projectSelection";
+import Loader from "../components/loader";
 
 export default function Projects() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Example function to simulate loading
+    useEffect(() => {
+        setTimeout(() => setIsLoading(false), 3000); // Simulate 3 seconds loading
+    }, []);
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <ProjectSelection/>
+        <Suspense fallback={<div>{isLoading ? <Loader /> : <div>Your Content Here</div>}</div>}>
+            <ProjectSelection />
         </Suspense>
     );
 }
