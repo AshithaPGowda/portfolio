b6b8e36a82a3c60f1c68b4b7cacd25f38fca77c2
Ashitha Gowda <pvagowda@yahoo.in>
2025-07-30 14:58:21 -0400

    updated my information

--- src/app/myInformation.js
+++ src/app/myInformation.js
@@ -1,11 +1,10 @@
 const CONSTANT = []
 
-CONSTANT.PARA1 = `I'm a full-stack developer who thrives on solving tough problems 🧩 and building high-performance systems 🚀. Backend engineering and APIs are my playground 🎯, and I love making things work seamlessly with Node.js, Next.js, React, and blockchain. At cnotes.in, I built a custom algorithm that predicts monthly billing for over 30,000 customers 💡— turning complexity into clarity. I'm always exploring how AI can push technology forward 🤖, and I thrive on collaboration, innovation, and challenges that make me think 🔥. When I'm not coding, I'm learning, experimenting, or finding new ways to level up 📚
-`
+CONSTANT.PARA1 = `I'm a BACKEND LEANING FULL STACK DEVELOPER driven by curiosity and a passion for solving complex challenges 🧩. I specialize in backend engineering and scalable APIs 🚀, crafting seamless systems with Node.js, Python, Next.js, React, and blockchain technologies. At cnotes.in, I built a custom algorithm that predicts monthly billing for 30,000+ customers 💡, transforming raw data into actionable insights. I'm fascinated by how AI can elevate technology 🤖 and constantly explore ways to build smarter, more resilient architectures. I thrive in collaborative environments that push boundaries, spark innovation, and demand creative problem-solving 🔥.`
 CONSTANT.PARA2 = ` 💬 I'm a strong believer in collaboration and empathy, always striving to uplift those around me. If
                     there's one thing I value deeply, it's meaningful connections and making a positive impact in every space
                     I occupy. 🎨 Outside work, you'll find me immersed in art, experimenting in the kitchen, or on a quest to
-                    discover the best local coffee shops. Life, for me, is about crafting beautiful experiences—both in and
-                    outside the code.`
+                    discover the best local coffee shops. Life, for me, is about crafting beautiful experiences, both in and
+                    outside of code.`
 CONSTANT.MY_NAME = "ASHITHA PARAMESHA GOWDA"
 export default CONSTANT;
